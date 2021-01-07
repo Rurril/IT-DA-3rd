4c279faf46615debea5cac37438d827116ae76f1
smkim104 <soomin30328@gmail.com>
2021-01-07 20:01:34 +0900

    Create BOJ 1915 김수민

---
+++ study/C++1/VCWeek1/BOJ_1915_김수민.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int a[1002][1002];
+char temp;
+int anw;
+
+int main()
+{
+	cin >> N >> M;
+	for (int i = 1; i <= N;i++) {
+		for (int j = 1; j <= M; j++) {
+			cin >> temp;
+			a[i][j] = temp - '0';
+		}
+	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (a[i][j] == 1) {
+				a[i][j] += min(a[i - 1][j], min( a[i][j - 1], a[i - 1][j - 1]));
+				anw = max(anw, a[i][j]);
+			}
+		}
+	}
+
+	cout << anw*anw<<"\n";
+}
+
