8a6bd03747335ebd5a44dd28c772dd0c0442785a
smkim104 <soomin30328@gmail.com>
2021-01-07 20:01:53 +0900

    Create BOJ 4172 김수민

---
+++ study/C++1/VCWeek1/BOJ_4172_김수민.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long x[1000001];
+int i;
+
+
+int main()
+{
+	
+	x[0] = 1;
+	for (int j=1; j<1000000; j++){
+			int v1 = floor(j - sqrt(j));
+			int v2 = floor(log(j));
+			int v3 = floor( j * sin(j) * sin(j));
+			x[j] = (x[v1] + x[v2]  + x[v3])%1000000;
+	}
+	while (true) {
+		cin >> i;
+		if (i == -1) {
+			break;
+		}
+		cout << x[i]<<"\n";
+	}
+}
+
