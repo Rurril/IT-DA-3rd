f5ee2b5acc4b0fe88fca1ec8636ddc2ca6cee573
WooWan <woohobi@gmail.com>
2021-01-22 13:45:59 +0900

    Update STD 14891 우창완

--- study/Python/VCWeek3/BOJ_14891_우창완.py
+++ study/Python/VCWeek3/BOJ_14891_우창완.py
@@ -2,7 +2,7 @@
 
 #2차원 배열을 만들어서 풀었으면 더 코드가 깔끔했을듯,,ㅍㅠㅠㅠㅠ
 #간단한 구현 문제
-
+#python의 장점인 짧은 코딩 적용 전후 버젼!
 
 def rotate(num, dirt):
 	visited[num] = True
@@ -35,6 +35,32 @@ def rotate(num, dirt):
 		for i in range(7):
 			wheel[num][i]=wheel[num][i+1]
 		wheel[num][7]=temp
+	
+	#효율적인 코딩
+	# if direction==1:
+	# 	#슬라이싱 꿀팁
+	# 	temp=wheel[num][-1]
+	# 	wheel[num][1:]=wheel[1][:-1]
+	# 	wheel[num][0]=temp
+	# else:
+	# 	temp=wheel[num][0]
+	# 	wheel[num][:-1]=wheel[num][1:]
+	# 	wheel[num][-1]=temp
+	#진짜 예술이다,,, 
+	# for x in [num+1, num-1]:
+    #     if 0 <= x < 4 and not visit[x]:
+    #         visit[x] = True
+    #         if x == (num+1):
+    #             if to_right != topni[x][6]:
+    #                 rotate(x, direction*-1)
+
+    #         else:
+    #             if to_left != topni[x][2]:
+    #                 rotate(x, direction*-1)
+
+
+	
+
 
 	
 wheel=[[0 for _ in range(8)] for _ in range(5)]
@@ -50,10 +76,12 @@ for i in range(k):
 	rotate(num , dir)
 sum=0
 
-if wheel[1][0]==1: sum+=1
-if wheel[2][0]==1: sum+=2
-if wheel[3][0]==1: sum+=4
-if wheel[4][0]==1: sum+=8
+# if wheel[1][0]==1: sum+=1
+# if wheel[2][0]==1: sum+=2
+# if wheel[3][0]==1: sum+=4
+# if wheel[4][0]==1: sum+=8
+for i in range(1,5):
+	sum+= 
 		
 	
 print(sum)
