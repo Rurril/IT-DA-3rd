7d70523642239af6de35a55e40912233d798b875
dayeon <cab0>
2021-02-04 16:53:16 +0900

    Create Hw 14940 백다연

--- study/C++1/VCWeek5/BOJ_14940_백다연.cpp
+++ study/C++1/VCWeek5/BOJ_14940_백다연.cpp
@@ -11,21 +11,20 @@
 using namespace std;
 
 int arr[1001][1001];
-int visit[1001][1001];
+//int visit[1001][1001];
 
 int x, y;
 int n, m;
-int cnt = 0;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 
 void bfs()
 {
-    queue <pair<int, int>> q;
+    queue<pair<int, int>> q;
 
-    q.push(make_pair(x,y));
-    visit[x][y] = 2;
+    q.push(make_pair(x, y));
+    arr[x][y] = 2;  
 
     while (!q.empty())
     {
@@ -41,10 +40,10 @@ void bfs()
 
             if (nx < 0 || nx >= n || ny < 0 || ny >= m)
                 continue;
-            if (arr[nx][ny] == 1)
+            if (arr[nx][ny] == 1) //갈수있는곳이면
             {
                 q.push({nx, ny});
-                arr[nx][ny] = arr[nx][ny] + 1;
+                arr[nx][ny] = arr[a][b] + 1; //갈수있는 곳에 +1
             }
         }
     }
@@ -59,24 +58,28 @@ int main()
         for (int j = 0; j < m; j++)
         {
             cin >> arr[i][j];
-            if (arr[i][j] == 2) //2인 곳 저장
-                x = j;
-            y = i;
-            if (arr[i][j] == 1)
-                cnt++;
+            if (arr[i][j] == 2) { //2인 곳 저장
+                x = i;
+                y = j;
+            }
+
         }
 
     bfs();
 
-        for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             int ans = arr[i][j];
-            if (ans - 2)
-                cout << ans;
+
+            if(ans)
+                cout << ans-2;
             else
+            {
                 cout << 0;
+            }
+            
         }
         cout << endl;
     }
