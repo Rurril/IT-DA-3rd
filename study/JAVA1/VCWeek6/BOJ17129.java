e754c0767adb91ee30fa463a8aa506db14ae60e3
HYUNindaeyo <43405705+HYUNindaeyo@users.noreply.github.com>
2021-02-25 20:37:49 +0900

    Create HW 17129 박소현

--- study/JAVA1/VCWeek6/BOJ17129.java
+++ study/JAVA1/VCWeek6/BOJ17129.java
@@ -4,7 +4,29 @@ public class BOJ17129 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+		int map[][] = new int[N][M];
+		int check[][] = new int[N][M];
+		int dy[] = {0,0,1,-1};
+		int dx[] = {1,-1,0,0};
+		int x = 0;
+		int y = 0;
+		for (int i=0;i<N;i++) {
+			String S = scan.next();
+			for (int j=0;j<M;j++) {
+				map[i][j] = S.charAt(j) - '0';
+				if (map[i][j] ==2) {
+					x = N; 
+					y = M;
+				}
+			}
 	}
-
+	public static int bfs() {
+		
+	}
+		
 }
+	//이것도 그냥 ,,,bfs 문제,,, 같은데 
+//그냥 세마리가 각자 갈길 가고 그 세개 중에 최소값을 구해보겠ㄷ가 저의 풀이입니다 ...!
